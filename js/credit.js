acb7fef00f1699143b377f3d565c2d71d2906bf0
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2021-08-05 17:54:34 +0100

    Update credit.js

--- js/credit.js
+++ js/credit.js
@@ -5,14 +5,13 @@
 /////////////////////////////////////////////////////////
 
 try {
-  var credits = document.getElementById("credits");
+  var credits = document.getElementById('credits';
 } catch(e) {
   throw("Error while getting \"credits\" DIV: "+e);
 }
 
 if (credits) {
-  var credit = '<div id="tj20201-credit-SRCSTYLEUSED" style="background-color: #101010;height: 128px; color: #d8d8d8;width: 50ch;padding: 1px;border-radius: 2px;
-"><img href="https://tj20201.github.io/src/img/srcicon.png" alt="" style="width:128px;height:128px;float:left;border-radius:5px;"><p> This site uses TJ\'s styles.<br> <span style="border-radius:3px;background-color:#202020;font-family: Consolas, monospace;padding: 1px;">src</span> git repository: <a href="https://github.com/TJ20201/src">Click</a></p></div>';
+  var credit = "<div id='tj20201-credit-SRCSTYLEUSED' style='background-color: #101010;height: 128px; color: #d8d8d8;width: 50ch;padding: 1px;border-radius: 2px;'><img href='https://tj20201.github.io/src/img/srcicon.png' alt='' style='width:128px;height:128px;float:left;border-radius:5px;'><p> This site uses TJ's styles.<br> <span style='border-radius:3px;background-color:#202020;font-family: Consolas, monospace;padding: 1px;'>src</span> git repository: <a href='https://github.com/TJ20201/src'>Click</a></p></div>";
   credits.insertAdjacentHTML('beforeend', credit);
 } else {
   throw("Put credit.js on your credits page, or if this is the credits page please create a \"credits\" DIV!");
